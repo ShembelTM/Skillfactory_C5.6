0ac4a5436ecf9d2428e41192a6f241e4e187f3d6
Юрий Филянов <164732782+ShembelTM@users.noreply.github.com>
2024-04-29 17:58:53 +0300

    Add files via upload

---
+++ TelegramBot.py
@@ -0,0 +1,41 @@
+import telebot
+from config import keys, TOKEN
+from extensions import ApiException, CryptoConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+                    #Текст выводимый командой start и help
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу, введите комманду боту в следующем формате:\n<имя валюты>  \
+<в какую валюту перевести> \
+<количество переводимой валюты>\nУвидить список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+                    #Текст выводимый командой values
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+                    #Вывод запроса
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ApiException('Слишком много параметров.')
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ApiException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '6955405323:AAEANz7zGi8--kXmGnMOtUVS3aZKyxXAjPQ'
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+}
